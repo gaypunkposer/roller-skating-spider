28bd2499a8f71d166d6d6618fb1bedd99b1e7e61
Jessica Coan <jcoan188@gmail.com>
2018-04-02 15:13:11 -0700

    first commit

---
+++ Assets/scripts/Follow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+	public Transform Target;
+	public Vector3 Offset;
+	public float FollowTime;
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Vector3 target = Vector3.Lerp(transform.position, Target.position, FollowTime);
+		target += Offset;
+		transform.position = target;
+	}
+}
---
+++ Assets/scripts/KeyToForce.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyToForce : MonoBehaviour {
+
+	public KeyCode Key;
+	public bool Relative;
+	public Vector3 DefaultForce;
+	public Vector3 PressedForce;
+
+	ConstantForce force;
+	void Start () {
+		force = GetComponent<ConstantForce>();
+	}
+
+	void Update () {
+		if (Input.GetKey(Key)){
+			UpdateForce(PressedForce);
+			//Debug.Log(Key.ToString() + " pressed");
+		}
+		else {
+			UpdateForce(DefaultForce);
+		}
+	}
+
+	void UpdateForce(Vector3 updatedForce) 
+	{
+		if (Relative) {
+			force.relativeForce = updatedForce;
+		}
+		else {
+			force.force = updatedForce;
+		}
+	}
+}
